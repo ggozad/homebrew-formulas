2e1e682a5e47d5931f790285e234285d26aee2c4
Yiorgis Gozadinos <ggozadinos@gmail.com>
2024-01-12 10:07:55 +0100

    Add extra dependencies for Pillow. Closes #1.

--- Formula/oterm.rb
+++ Formula/oterm.rb
@@ -8,6 +8,9 @@ class Oterm < Formula
 
   depends_on "python3"
   depends_on "openjpeg"
+  depends_on "pkg-config"
+  depends_on "jpeg-turbo"
+  depends_on "zlib"
 
   resource "aiohttp" do
     url "https://files.pythonhosted.org/packages/54/07/9467d3f8dae29b14f423b414d9e67512a76743c5bb7686fb05fe10c9cc3e/aiohttp-3.9.1.tar.gz"
